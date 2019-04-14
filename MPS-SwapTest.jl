073dcc2182f3642382a31eb64e1b66d1bc029c12
frankwangws <frank.wang.w.s@gmail.com>
2019-04-15 00:42:32 +0800

    BUG fixed

--- MPS-SwapTest.jl
+++ MPS-SwapTest.jl
@@ -5,48 +5,49 @@ using LinearAlgebra
 using Random
 using Test
 
-#==#
-"""Auther Tests only. """ 
-push!(LOAD_PATH,"../MPSDiffCircuit.jl/src")
-#==#
 """
+Using Matrix Product State Structure to test state overlaps between 2 wave-functions.
+
 Need to add another package if you want to run the code: 
 https://github.com/frankwswang/MPSDiffCircuit.jl.git
 """
-# =#
-using MPSDiffCircuit 
 
-function MScircuit(nBitT::Int64, vBit::Int64, depth::Int64, ϕ::Real)
+#==#
+# Author Tests only.  
+push!(LOAD_PATH,"../MPSDiffCircuit.jl/src") 
+# =#
+
+using MPSDiffCircuit
+
+function MScircuit(nBitT::Int64, vBit::Int64, depth::Int64, ϕ::Real, MPSblocks::Array)
     nBitG = 1 + vBit
     nBitA = 1 + nBitG + nBitT 
     Cblocks = []
-    MPSblocks = []
     push!(Cblocks, put(nBitA, nBitA=>H))
     push!(Cblocks, put(nBitA, nBitA=>shift(ϕ)))
-    println("s1")
-    MPSGen = MPSDC(depth, nBitT, vBit)
-    println("s1_2")
+    #println("s1")
     for i = 1:(nBitT-vBit)
-        push!( MPSblocks, MPSGen.cBlocks[i] ) 
-        println("s2_1")
         MPSblock = put(nBitA, Tuple( (nBitA-1):-1:(nBitT+1), )=>MPSblocks[i] )
-        println("s2_2")
+        #println("s2")
         push!(Cblocks, MPSblock)
         SWAPblock = control(nBitA, nBitA, ( (nBitA-1), (nBitT+1-i) )=>SWAP)
         push!(Cblocks, SWAPblock)
-        println("s3")
+        #println("s3")
     end
-    println("s4")
+    #println("s4")
     SWAPvBit = chain(nBitA, [control(nBitA, nBitA, ( (nBitT+i),i )=>SWAP) for i=vBit:-1:1])
-    println("s5")
+    #println("s5")
     push!(Cblocks, SWAPvBit)
-    println("s6")
     push!(Cblocks, put(nBitA, nBitA=>H))
-    println("s7")
+    #println("s6")
     circuit = chain(nBitA,Cblocks)
-    #dispatch!(circuit[2], ϕ)
-    println("s8")
+    #println("s7")
     circuit
+end
+
+function MSTest(nBitT::Int64, vBit::Int64, depth::Int64, ϕ::Real)
+    MPSGen = MPSDC(depth, nBitT, vBit)
+    MScircuit(nBitT, vBit, depth, ϕ, MPSGen.cBlocks)
 end
 
 nBitT = 4
@@ -54,6 +55,6 @@ vBit = 2
 depth = 1
 ϕ = 0
 
-c2 = MScircuit(nBitT, vBit, depth, ϕ)
+c2 = MSTest(nBitT, vBit, depth, ϕ)
 
 
