a3e0ef2fb9184f255fb4dcd3ceb017a75f3fd8d9
frankwswang <frank.wang.w.s@gmail.com>
2020-06-27 14:52:27 -0400

    Test.

--- src/MSQR.jl
+++ src/MSQR.jl
@@ -5,12 +5,11 @@ using QMPS
 using Flux
 using LinearAlgebra
 using Statistics
-using Test
-using Requires
+# using Requires
 
 include("MPSSwapTest.jl")
 include("MSQRtrain.jl")
 include("SWAPtrain.jl")
-@init @require CuYao="b48ca7a8-dd42-11e8-2b8e-1b7706800275" include("CUDA.jl")
+# @init @require CuYao="b48ca7a8-dd42-11e8-2b8e-1b7706800275" include("CUDA.jl")
 
 end
